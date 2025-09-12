9d1432f744ef109b1f2c20f871a6e4fe5ce23aba
Vincent Wang <vincent.wang.60@gmail.com>
2025-09-12 16:32:35 -0400

    shader fun

--- assets/fragment.glsl
+++ assets/fragment.glsl
@@ -11,75 +11,71 @@ vec3 hsv2rgb(vec3 c) {
 }
 
 vec2 rings(vec2 _uv, float time, float seed) {
-    float safe_time = time + seed;
-
-    float factor1 = .05, freq1 = 1.0;
-    float sin1 = sin(freq1 * _uv.y + safe_time);
-    float cos1 = cos(freq1 * _uv.x + safe_time);
-    _uv.x += factor1 * sin1;
-    _uv.y += factor1 * cos1;
+    float time_seed = time + seed;
+    float sin1 = sin(_uv.y + time_seed), cos1 = cos(_uv.x + time_seed);
+    _uv += 0.05 * vec2(sin1, cos1);
     
-    float factor2 = 0.02, freq2 = 10.0 + seed;
-    float sin2 = sin(freq2 * _uv.y + safe_time);
-    float cos2 = cos(freq2 * _uv.x + safe_time);
-    _uv.x += factor2 * sin2;
-    _uv.y += factor2 * cos2;
+    float freq2 = 10.0 + seed;
+    float sin2 = sin(freq2 * _uv.y + time_seed), cos2 = cos(freq2 * _uv.x + time_seed);
+    _uv += 0.02 * vec2(sin2, cos2);
     
     return _uv;
 }
 
 void getVec2Array(out vec2 arr[7]) {
     for (int i = 0; i < 6; ++i) {
-        float r = 0.05 + 0.01 * sin(u_time + float(i) * 0.5);
-        float angle = 2.0 * PI * float(i) / 6.0 + 0.1 * (u_time) + 0.05 * sin(u_time + float(i) * 2.5);
-        arr[i] = vec2(r * cos(angle), r * sin(angle));
+        float r = 0.1 + 0.001 * sin(u_time + float(i) * 0.5);
+        float angle = 2.0 * PI * float(i) / 6.0 + 0.05 * sin(u_time + float(i) * 2.5) + PI / 6.0;
+        arr[i] = r * vec2(cos(angle), sin(angle));
     }
-    arr[6] = vec2(0.0, 0.0);
+    arr[6] = vec2(0.0);
 }
 
-
-float voronoi( in vec2 uv, in vec2 mouse )
-{
+vec3 voronoi(vec2 uv) {
     vec2 points[7];
     getVec2Array(points);
-    float minDist = 100.0;
-    int closestIndex = 0;
-    
+    float d1 = 1e9, d2 = 1e9;
+    int idx = 0;
     for (int i = 0; i < 7; ++i) {
-        float dist = length(uv - points[i]);
-        if (dist < minDist) {
-            minDist = dist;
-            closestIndex = i;
+        float d = length(uv - points[i]);
+        if (d < d1) {
+            d2 = d1; d1 = d; idx = i;
+        } else if (d < d2) {
+            d2 = d;
         }
     }
-    return float(closestIndex + 1);
+    return vec3(d1, d2, float(idx));
 }
 
 void main() {
-    float time = u_time * .5;
-    float scale = 2.5 / (min(u_resolution.x, u_resolution.y));
+    float time = u_time * 0.5;
+    float scale = 2.5 / min(u_resolution.x, u_resolution.y);
     vec2 mouse_norm = u_mouse / u_resolution;
-    float seed = (mouse_norm.x + mouse_norm.y);
+    float seed = mouse_norm.x + mouse_norm.y;
     vec2 pos = gl_FragCoord.xy - u_resolution * 0.5;
     float radius = length(pos) * scale;
-    float hue = 0.5 + .5 * sin(0.1 *u_time);
     float angle = fract(atan(pos.y, pos.x) / (2.0 * PI));
     
     pos = radius * vec2(cos(angle * 2.0 * PI), sin(angle * 2.0 * PI));
     vec2 mask_uv = rings(pos, time, seed);
     mask_uv *= mask_uv;    
     float intensity = fract(mask_uv.x + mask_uv.y);
-    vec3 bg_color = hsv2rgb(vec3(hue + radius * .2, 1.0, 0.25 * intensity));
+    vec3 bg_color = hsv2rgb(vec3(0.10, 0.6 + 0.15 * radius, (0.5 - 0.12 * radius) * intensity));
     
-    float c = voronoi(pos, mouse_norm);
+    vec3 vinfo = voronoi(pos);
+    float c = vinfo.z + 1.0;
     vec3 voronoi_color = c == 7.0 
-        ? vec3(0.0, 0.0, 0.0) 
-        : hsv2rgb(vec3(0.5 + 0.1 * mod(c, 3.0), 0.5, 0.2));
+        ? vec3(0.0) 
+        : hsv2rgb(vec3(0.11, 1.0 - 0.10 * mod(c, 3.0), 0.6 + 0.10 * mod(c, 3.0)));
+    
+    float diff = vinfo.y - vinfo.x;
+    float edge = 1.0 - smoothstep(0.001, 0.002, abs(diff * radius));
+    voronoi_color = mix(voronoi_color, vec3(0.0), edge);
     
     vec3 final_color = 
         (radius < 0.8) ? voronoi_color :
         (radius > 1.1) ? bg_color :
-        (intensity < 0.5 ? bg_color : voronoi_color);
+        (intensity < 0.5 ? bg_color : mix(voronoi_color, bg_color, smoothstep(0.8, 1.2, intensity)));
     
     gl_FragColor = vec4(final_color, 1.0);
 }
---
+++ server.ps1
@@ -0,0 +1,2 @@
+Set-Location $PSScriptRoot
+python -m http.server 8000 --bind 127.0.0.1
